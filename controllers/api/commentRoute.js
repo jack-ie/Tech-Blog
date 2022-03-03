6054d7bc0ea01704eb8cf3df6a6e51f2ad0175c4
jack-ie <jackie919@outlook.com>
2022-03-03 14:39:11 -0600

    updated server

---
+++ controllers/api/commentRoute.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id,
+            post_id: req.body.post_id
+        });
+        // it will return to the commented post
+        res.redirect(req.header('Refer'));
+        } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+
+module.exports = router;
